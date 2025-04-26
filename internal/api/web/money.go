7e146d3bc4d308e191a841c90927ce68466ec99e
fredy.diaz <fredy.diaz@2degrees.nz>
2025-04-27 09:19:24 +1200

    feat: rename resources

--- internal/api/web/money.go
+++ internal/api/web/money.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 
 	"github.com/fhdiaze/frugi/internal/core/money"
-	"github.com/fhdiaze/frugi/static"
+	"github.com/fhdiaze/frugi/rsrc"
 	"github.com/labstack/echo/v4"
 )
 
 var (
-	convertGetTemplate = template.Must(template.ParseFS(static.TemplatesFS, "templates/comps/frame.html", "templates/money/convert.html"))
+	convertGetTemplate = template.Must(template.ParseFS(rsrc.TemplatesFS, "templates/comps/frame.html", "templates/money/convert.html"))
 	convertOutTemplate = template.Must(template.ParseFS(static.TemplatesFS, "templates/comps/frame.html", "templates/money/convert_out.html"))
 )
 
