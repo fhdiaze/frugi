fbaa9f73080ed7454753bddb9fdac77a2d2fe77a
fredy.diaz <fredy.diaz@2degrees.nz>
2025-05-04 11:38:21 +1200

    feat: add compound

--- internal/web/money.go
+++ internal/web/money.go
@@ -10,14 +10,17 @@ import (
 )
 
 var (
-	convertGetTemplate = template.Must(template.ParseFS(resources.TemplatesFS, "templates/comps/frame.html", "templates/money/convert.html"))
-	convertOutTemplate = template.Must(template.ParseFS(resources.TemplatesFS, "templates/comps/frame.html", "templates/money/convert_out.html"))
+	convertGetTemplate  = template.Must(template.ParseFS(resources.TemplatesFS, "templates/comps/frame.html", "templates/money/convert.html"))
+	convertOutTemplate  = template.Must(template.ParseFS(resources.TemplatesFS, "templates/comps/frame.html", "templates/money/convert_out.html"))
+	compoundGetTemplate = template.Must(template.ParseFS(resources.TemplatesFS, "templates/comps/frame.html", "templates/money/compound.html"))
+	compoundOutTemplate = template.Must(template.ParseFS(resources.TemplatesFS, "templates/comps/frame.html", "templates/money/compound_out.html"))
 )
 
 func RouteMoney(group *echo.Group) {
 	group.GET("/money.convert.get", handleGetConvert)
 	group.POST("/money.convert.run", handleRunConvert)
 	group.GET("/money.compound.get", handleGetCompound)
+	group.POST("/money.compound.run", handleRunCompound)
 }
 
 func handleGetConvert(context echo.Context) error {
@@ -46,5 +49,12 @@ func handleGetCompound(context echo.Context) error {
 }
 
 func handleRunCompound(context echo.Context) error {
-	return nil
+	var cmd commands.RunCompoundCmd
+	if err := context.Bind(&cmd); err != nil {
+		return err
+	}
+
+	result, err := commands.HandleRunCompound(&cmd)
+
+	return err
 }
